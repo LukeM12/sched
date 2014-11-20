5457ef6d601f898502fa93278436a1bc1a9d6d87
Luke Morrison <luc785@hotmail.com>
2014-11-20 12:19:13 -0500

    Started reconsutuction and architecture. Hiding divs are replaced by ajax calls

--- controller/Pages/PageControl.js
+++ controller/Pages/PageControl.js
@@ -3,7 +3,22 @@
   //This is basically the Page that loads All The various rudimentary navigation
   function LoadMainPage()
     {
-    var xmlhttp;
+      var xmlhttp;
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp=new XMLHttpRequest();
+      }
+    else {// code for IE6, IE5
+      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+      }
+    xmlhttp.onreadystatechange = function(){
+      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+        document.getElementById("MainBlock").innerHTML=xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","view/pages/nav_page.html",true);
+    xmlhttp.send();
+   /* var xmlhttp;
     if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
       }
@@ -17,8 +32,9 @@
         }
     }
     xmlhttp.open("GET","view/pages/home_page.html",true);
-    xmlhttp.send();
+    xmlhttp.send();*/
     }
+    /* This page loads and displays the content in the login page */
     function LoadLoginPage()
         {
         var xmlhttp;
