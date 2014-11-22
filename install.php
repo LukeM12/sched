83239845ebd017b9b1c6dfe50790d1ea73c14526
volodymyrsharovar <volodymyrsharovar@gmail.com>
2014-11-21 23:58:47 -0500

    Added functions to populate courses_Needed table
    -Removed PRIMARY KEY(courseID) from courses table because it conflicted
    with populating it from a file (multiple duplicates of courseID)
    -Added a function to populate courses_Needed table

--- install.php
+++ install.php
@@ -28,11 +28,10 @@
      **/
 
     function initTables($DB){
-        
         //Make the courses table
         //The prereqs are implicit in a string
         $sql = "CREATE TABLE IF NOT EXISTS course(
-                    subject text NOT NULL,
+                    subject varchar(4) NOT NULL,
                     courseID varchar(255) NOT NULL,
                     sequence varchar(2) NOT NULL,
                     catalog_title varchar(255) NOT NULL,
@@ -40,11 +39,10 @@
                     days varchar(5) NOT NULL,
                     startTime varchar(4) NOT NULL,
                     endTime varchar(4) NOT NULL,
-                    room_cap int NOT NULL,
-                    PRIMARY KEY(courseID)
+                    room_cap int NOT NULL
              );";
         $DB->execute($sql);
-
+        echo $DB->getError();
        //Make the student table  
         $sql = "CREATE TABLE IF NOT EXISTS student(
                     studentID int NOT NULL,
@@ -59,33 +57,34 @@
         //This table is actually coupled between student and course, and there will be copies of both the student and the course but not combined
         $sql = "CREATE TABLE IF NOT EXISTS courses_Taken(
                     studentID int NOT NULL,
-                    courseID varchar(255) NOT NULL,
-                    FOREIGN KEY (studentID) references student(studentID),
-                    FOREIGN KEY (courseID) references course(courseID)
-            );";
+                    subject varchar(4) NOT NULL,
+                    courseID varchar(255) NOT NULL);";
+
         $DB->execute($sql);
+        echo $DB->getError();
         //Make the courses that the student needs to take table
         //It is similar to courses_Taken table but instead of having the courses the student took it will contain 
         //the courses the student needs to take
         $sql = "CREATE TABLE IF NOT EXISTS courses_Needed(
                     studentID int NOT NULL,
+                    subject varchar(4) NOT NULL,
                     courseID varchar(255) NOT NULL,
-                    entry int,
-                    FOREIGN KEY (studentID) references student(studentID),
-                    FOREIGN KEY (courseID) references course(courseID)
-            );";
+                    entry int);";
+            
         $DB->execute($sql);
+        echo $DB->getError();
         //Make a communication engineering required courses table
         //Table contains the courses required by the communication engineering
         //Table is coupled with course table
         $sql = "CREATE TABLE IF NOT EXISTS CE_program(
                     year int NOT NULL,
+                    subject varchar(4) NOT NULL,
                     courseID varchar(255) NOT NULL,
                     term char NOT NULL,
-                    PRIMARY KEY (courseID)
-                );";
+                    PRIMARY KEY (courseID));";
+                
         $DB->execute($sql);
-        $DB->getError();
+        echo $DB->getError();
         //Had an issue when populating the database when it had the foreign key
         
         /*$sql = "CREATE TABLE IF NOT EXISTS CE_program(
@@ -96,42 +95,5 @@
                     FOREIGN KEY (courseID) references course(courseID)
         );";
         $DB->execute($sql);*/
-        //testInitTables($DB);
-    }
-
-    /*
-     * Description: Create the necessary tables for the site
-     * param: A live instance of the database
-     * return: implicit -> the tables are now produced //Return Bool?
-     **/
-    function testInitTables($DB) {
-        //set up
-        $sql = "INSERT INTO course(courseID, days, startTime, endTime) values ('ELEC2501', 'M,W' ,'835','1035');";
-        $DB->execute($sql);
-        $DB->getError();
-        
-        $sql = "INSERT INTO course(courseID, days, startTime, endTime) values ('ECOR1101', 'T,R' ,'935','1135');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO course(courseID, days, startTime, endTime) values ('SYSC2004', 'M,W' ,'1135','1435');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO course(courseID, days, startTime, endTime) values ('SYSC2002', 'T,R' ,'1135','1435');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO student(studentID, name, onCourse, password, coursesTaken) values (223, 'Jim', 'YES', 'Pass', 'ECOR1101,ELEC2501,SYSC2004,SYSC2002');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO courses_Taken(studentID, courseID) values ('223', 'ECOR1101');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO courses_Taken(studentID, courseID) values ('223', 'ELEC2501');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO courses_Taken(studentID, courseID) values ('223', 'SYSC2004');";
-        $DB->execute($sql);
-        
-        $sql = "INSERT INTO courses_Taken(studentID, courseID) values ('223', 'SYSC2002');";
-        $DB->execute($sql);
     }
 ?>
