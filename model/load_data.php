58b8bf9395179c099acf42c71111524c11601961
Luke Morrison <luc785@hotmail.com>
2014-11-27 18:15:46 +0000

    Created a multi-platform solution to parsing data

--- model/load_data.php
+++ model/load_data.php
@@ -2,65 +2,76 @@
     //To load the script run "http://localhost/model/load_data.php"
 
     function loadCSVfiles($DB) {
-        /**
-        * Uncomment this section to parse the files on Linux box and comment out the 
-        * Windows section
-        */
-        /*
-	    $sql=   'LOAD DATA LOCAL INFILE "/opt/lampp/htdocs/model/course_data.csv"                 
+        $dir = getcwd();
+        
+        $pieces = explode("/", $dir);
+        $size_path = sizeof($pieces);
+        $OS = '';
+
+        if ( $size_path == 1 ){
+            $pieces = explode("\\", $dir);
+		    $size_path = sizeof($pieces);
+		    $OS = $OS.'WINDOWS';
+		}
+		else {
+			$OS = $OS.'LINUX';
+		}
+        $final_path = '';
+		$ce_program = ''; //this array later on
+		$courses = '';
+		if ( strcmp($OS, "LINUX") == 0 ){ 
+			$i=0;
+			while( strcmp($pieces[$i], 'controller') != 0){
+				if ($i==0){
+					$final_path = $pieces[$i];
+				}	
+				else{
+					$final_path = $final_path.'/'.$pieces[$i];
+				}			
+				$i++;
+			}
+			$final_path = $final_path.'/';
+            $courses= $final_path.'model/course_data.csv';
+            $ce_program = $final_path.'model/ce_program.csv';
+		}
+		//Data filepath reconstruction on Linux Operating Systems
+		if ( strcmp($OS, "WINDOWS") == 0 ){  	
+			$i=0;
+			while( strcmp($pieces[$i], 'controller') != 0){
+				if ($i==0){
+					$final_path = $pieces[$i];
+				}	
+				else{
+					$final_path = $final_path.'/'.$pieces[$i];
+				}			
+				$i++;
+			}
+			$final_path = $final_path.'/';
+			$courses= $final_path.'model/course_data.csv';
+            $ce_program = $final_path.'model/ce_program_c.csv';
+		}
+
+        $enclosed =  '"';
+	    $sql=   'LOAD DATA LOCAL INFILE "'.$courses.'"                 
 		        INTO TABLE course 
 		        FIELDS 
 		        	TERMINATED BY ";" 
 		        	ENCLOSED BY "\""
 		        		LINES TERMINATED BY "\n" IGNORE 1 LINES';
-                
-        $DB->execute($sql);
-        echo $DB->getError();
-        
-        $sql = 'LOAD DATA LOCAL INFILE "/opt/lampp/htdocs/model/ce_program_course_data.csv"
-                INTO TABLE ce_program
-                FIELDS 
-                    TERMINATED BY ";"
-                LINES 
-                	TERMINATED BY "\n";';
 
+        //Up until now, the path is effectively the right path to the data, and that is validated
         $DB->execute($sql);
         echo $DB->getError();
-        */
-        
-        /**
-        * Uncomment this section to parse the files on Windows box and comment out the 
-        * Linux section
-        */
         $enclosed =  '"';
-        $sql = "LOAD DATA LOCAL INFILE '../model/course_data.csv'
-                INTO TABLE course
-                FIELDS 
-                    TERMINATED BY ';'
-                    ENCLOSED BY '".$enclosed."'
-                LINES 
-                    TERMINATED BY '\r\n'
-                IGNORE 1 LINES;";
-                
-        $DB->execute($sql);
-        echo $DB->getError();
-        
-        $sql = "LOAD DATA LOCAL INFILE '../model/ce_program.csv'
-                INTO TABLE ce_program
-                FIELDS 
-                    TERMINATED BY ';'
-                LINES 
-                    TERMINATED BY '\r\n';";
-        $DB->execute($sql);
+	    $sql=   'LOAD DATA LOCAL INFILE "'.$ce_program.'"                 
+		        INTO TABLE ce_program
+		        FIELDS 
+		        	TERMINATED BY ";" 
+		        		LINES TERMINATED BY "\n" IGNORE 1 LINES';
+
+       $DB->execute($sql);
         echo $DB->getError();
     }
-
-    //mysql> LOAD DATA LOCAL INFILE '/opt/lampp/htdocs/model/course_data.csv'                 INTO TABLE course FIELDS TERMINATED BY ';' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 LINES;
-
-    //testInitTables($connection);
-    //$studentID = 223;
-    //populateCoursesNeeded($connection, $studentID);
-
     /*
      * Description: Create the necessary tables for the site
      * param: A live instance of the database
@@ -97,29 +108,5 @@
         //echo mysqli_error($connection);
         $DB->execute($sql);
         echo $DB->getError();
-        
-        /*$sql = "SELECT * FROM courses_Taken WHERE courseID='223';";
-        $result = $connection->query($sql);
-        echo mysqli_error($connection);
-        $rows = $result->num_rows;
-        if($rows==0){
-            $sql = "INSERT INTO courses_Taken(studentID, subject, courseID) values ('223', 'ECOR', '1101');";
-            $connection->query($sql);
-            echo mysqli_error($connection);
-            
-            $sql = "INSERT INTO courses_Taken(studentID, subject, courseID) values ('223', 'MATH', '1104');";
-            $connection->query($sql);
-            echo mysqli_error($connection);
-            
-            $sql = "INSERT INTO courses_Taken(studentID, subject, courseID) values ('223', 'SYSC', '2004');";
-            $connection->query($sql);
-            echo mysqli_error($connection);
-            
-            $sql = "INSERT INTO courses_Taken(studentID, subject, courseID) values ('223', 'SYSC', '2001');";
-            $connection->query($sql);
-            echo mysqli_error($connection);
-        }*/
     }
-    
-    
 ?>
