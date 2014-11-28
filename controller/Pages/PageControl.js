17f2913ac3a771d26a21e0306cec91ef36ade5a6
Luke Morrison <luc785@hotmail.com>
2014-11-27 19:42:21 -0500

    Isolating Ajax Modules to allow space for logic and view

--- controller/Pages/PageControl.js
+++ controller/Pages/PageControl.js
@@ -1,96 +1,103 @@
-  
+/**
+ * Author : Luke Morrison
+ * email : lukemorrison@cmail.carleton.ca
+ */
 
-  //This is basically the Page that loads All The various rudimentary navigation
-  function LoadMainPage()
-    {
-      var xmlhttp;
+/*************************************************
+				HTML VIEW CONTENT
+ */
+
+/**
+ * Description: Load the Entire Page Manually
+ * return: The HTML content for the main page
+ */
+function LoadMainPage(){
+	var xmlhttp;
     if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-      }
-    else {// code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-        document.getElementById("Navigation").innerHTML=xmlhttp.responseText;
-        }
+    	xmlhttp=new XMLHttpRequest();
     }
+    else {// code for IE6, IE5
+    	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = 
+    	function(){
+	    	if (xmlhttp.readyState==4 && xmlhttp.status==200){
+	    		document.getElementById("Navigation").innerHTML=xmlhttp.responseText;
+	        }
+    	}
     xmlhttp.open("GET","view/pages/nav_page.html",true);
     xmlhttp.send();
-   /* var xmlhttp;
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-      }
-    else {// code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-        document.getElementById("MainBlock").innerHTML=xmlhttp.responseText;
-        }
     }
-    xmlhttp.open("GET","view/pages/home_page.html",true);
-    xmlhttp.send();*/
+/**
+ * Description: Load the Page for signing up, into the main Div
+ * return: The HTML content for the sign-up page
+ */
+function LoadSignupPage(){
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = 
+		function(){
+			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+				document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
+		    }
+		}
+	xmlhttp.open("GET","view/pages/signup_page.html",true);
+	xmlhttp.send();
+	}
+/**
+ * Description: Load the Page for Logging in, into the main Div
+ * return: The HTML content for the login page
+ */
+function LoadLoginPage(){
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
     }
-    /* This page loads and displays the content in the login page */
-    function LoadSignupPage()
-        {
-        var xmlhttp;
-        if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-          xmlhttp=new XMLHttpRequest();
-          }
-        else {// code for IE6, IE5
-          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-          }
-        xmlhttp.onreadystatechange = function(){
-          if (xmlhttp.readyState==4 && xmlhttp.status==200)
-            {
-            document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET","view/pages/signup_page.html",true);
-        xmlhttp.send();
-        }
-    function LoadLoginPage()
-    {
-      var xmlhttp;
-      if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-        }
-      else {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-        }
-      xmlhttp.onreadystatechange = function(){
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-          {
-          document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
-          }
-      }
-      xmlhttp.open("GET","view/pages/login_page.html",true);
-      xmlhttp.send();
+	else {// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
-    function SubmitForm()
-    {
-    var xmlhttp;
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-      }
-    else {// code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
-    xmlhttp.onreadystatechange = function(){
-      if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-        document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
-        }
-    }
- //   xmlhttp.open("POST","controller/login.php",true);
-        document.getElementById("MainContent").innerHTML="Did ti worl";
-    xmlhttp.open("POST","controller/login.php",true);
-    alert("Did it work");
-    xmlhttp.send();
-    }
+	xmlhttp.onreadystatechange = 
+		function(){
+			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+			  document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
+			}
+		}
+	xmlhttp.open("GET","view/pages/login_page.html",true);
+	xmlhttp.send();
+}
+
+/*************************************************
+				PHP BACKEND
+*/
+
+/**
+ * Description: Load the Page for Logging in, into the main Div
+ * return: The HTML content for the login page
+ */
+function SubmitForm(){
+	var xmlhttp;
+	if (window.XMLHttpRequest) {// IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else {// IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange = 
+		function()
+		{
+			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+				document.getElementById("MainContent").innerHTML=xmlhttp.responseText;
+		    }
+		}
+	document.getElementById("MainContent").innerHTML="Did ti worl";
+	xmlhttp.open("POST","controller/login.php",true);
+	alert("Did it work");
+	xmlhttp.send();
+}
 
 
